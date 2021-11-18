963577d1b905b97c34ffbad37a1b304b70ac6b9f
Yuragranovsky <yuragranovsky@gmail.com>
2021-11-18 10:13:31 +0200

    cw #13 method remove that should be rewritten in HW #13

--- src/telran/util/TreeSet.java
+++ src/telran/util/TreeSet.java
@@ -49,6 +49,10 @@ Node<T> current = root == null ? root : getMostLeftFrom(root);
 			getFirstParentGreater(current);
 		return res;
 	}
+	@Override 
+	public void remove() {
+		//TODO
+	}
 	 
  }
 	@Override
@@ -95,12 +99,68 @@ Node<T> current = root == null ? root : getMostLeftFrom(root);
 	}
 	@Override
 	public T remove(T pattern) {
-		// TODO next HW
-		return null;
+		Node<T> removedNode = getNode(pattern);
+		if (removedNode == null) {
+			return null;
+		}
+		removeNode(removedNode);
+		return removedNode.obj;
 	}
 
 	
 
+	private void removeNode(Node<T> removedNode) {
+		//TODO update the method by applying another algorithm
+		if (removedNode == root) {
+			removeRoot();
+		} else {
+			Node<T> parent = removedNode.parent;
+			Node<T> child = removedNode.right == null ? removedNode.left : removedNode.right;
+			
+			if (parent.right == removedNode) {
+				parent.right = child;
+				
+			} else {
+				parent.left = child;
+			}
+			if (child != null) {
+				child.parent = parent;
+			}
+			if (removedNode.right != null) {
+				Node<T> parentLeft = getMostLeftFrom(removedNode.right);
+				parentLeft.left = removedNode.left;
+				if(removedNode.left != null) {
+					removedNode.left.parent = parentLeft;
+				}
+				
+			}
+		}
+		size--;
+		
+	}
+	private void removeRoot() {
+		//TODO update the method by applying another algorithm (see slide 28)
+		Node<T> child = root.right == null ? root.left : root.right;
+		if (child != null) {
+			child.parent = null;
+		}
+		if (root.right != null) {
+			Node<T> parentLeft = getMostLeftFrom(root.right);
+			parentLeft.left = root.left;
+			if (root.left != null) {
+				root.left.parent = parentLeft;
+			}
+		}
+		root = child;
+		
+	}
+	private Node<T> getNode(T pattern) {
+		Node<T> current = root;
+		while(current != null && !current.obj.equals(pattern)) {
+			current = comp.compare(pattern, current.obj) > 0 ? current.right : current.left;
+		}
+		return current;
+	}
 	@Override
 	public Iterator<T> iterator() {
 		
